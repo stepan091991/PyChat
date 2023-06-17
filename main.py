52d25632ab36fce7f74bfebdc914193ff67a6e96
stepan091991 <stepan.fedorov00@mail.ru>
2023-06-17 22:49:47 +0500

    ver1

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 import customtkinter
+customtkinter.set_appearance_mode("dark")
+def login_button_click():
+    print("button click")
 
+def register_button_click():
+    print("button click")
 
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
         self.geometry("600x500")
         self.title("CTk example")
-        self.configure(fg_color="red")
+        self.resizable(width=False, height=False)
 
         # add widgets to app
-        self.login_button = customtkinter.CTkButton(self, command=self.button_click)
-        self.login_button.grid(row=0, column=0, padx=20, pady=10)
-        self.registration_button = customtkinter.CTkButton(self, command=self.button_click)
-        self.registration_button.grid(row=1, column=0, padx=20, pady=30)
+        self.checkbox_frame = customtkinter.CTkFrame(self)
+        self.checkbox_frame.place(x=215,y=180)
+        self.login_entry = customtkinter.CTkEntry(self.checkbox_frame, placeholder_text="Логин")
+        self.login_entry.grid(row=0, column=0, padx=20, pady=11)
+        self.registration_entry = customtkinter.CTkEntry(self.checkbox_frame, placeholder_text="Пароль")
+        self.registration_entry.grid(row=1, column=0, padx=20, pady=11)
+        self.login_button = customtkinter.CTkButton(self.checkbox_frame, command=login_button_click,text="Вход")
+        self.login_button.grid(row=2, column=0, padx=20, pady=11)
+        self.registration_button = customtkinter.CTkButton(self.checkbox_frame, command=register_button_click,text="Регистрация")
+        self.registration_button.grid(row=3, column=0, padx=20, pady=11)
+        self.version_text = customtkinter.CTkLabel(self, text="Version 0.1", fg_color="transparent")
+        self.version_text.place(x=7, y=475)
 
     # add methods to app
-    def button_click(self):
-        print("button click")
-
 
 app = App()
 app.mainloop()
