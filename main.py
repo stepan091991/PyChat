329b159779d284cb128364dbe36c455786da0e91
stepan091991 <stepan.fedorov00@mail.ru>
2023-06-17 23:15:26 +0500

    ver2

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 import customtkinter
 customtkinter.set_appearance_mode("dark")
-def login_button_click():
-    print("button click")
-
-def register_button_click():
-    print("button click")
 
 class App(customtkinter.CTk):
     def __init__(self):
@@ -20,14 +15,21 @@ class App(customtkinter.CTk):
         self.login_entry.grid(row=0, column=0, padx=20, pady=11)
         self.registration_entry = customtkinter.CTkEntry(self.checkbox_frame, placeholder_text="Пароль")
         self.registration_entry.grid(row=1, column=0, padx=20, pady=11)
-        self.login_button = customtkinter.CTkButton(self.checkbox_frame, command=login_button_click,text="Вход")
+        self.login_button = customtkinter.CTkButton(self.checkbox_frame, command=self.login_button_click,text="Вход")
         self.login_button.grid(row=2, column=0, padx=20, pady=11)
-        self.registration_button = customtkinter.CTkButton(self.checkbox_frame, command=register_button_click,text="Регистрация")
+        self.registration_button = customtkinter.CTkButton(self.checkbox_frame, command=self.register_button_click,text="Регистрация")
         self.registration_button.grid(row=3, column=0, padx=20, pady=11)
-        self.version_text = customtkinter.CTkLabel(self, text="Version 0.1", fg_color="transparent")
+        self.version_text = customtkinter.CTkLabel(self, text="Version 0.1", fg_color="transparent",state="disabled")
         self.version_text.place(x=7, y=475)
-
+        self.info_text = customtkinter.CTkLabel(self, text="", fg_color="transparent",anchor="center")
+        self.info_text.place(x=217, y=385)
     # add methods to app
+    def login_button_click(self):
+        self.info_text.configure(text="Неверный логин или пароль!")
+        self.info_text.place(x=217, y=385)
 
+    def register_button_click(self):
+        self.info_text.configure(text="Сервер не доступен!")
+        self.info_text.place(x=242, y=385)
 app = App()
 app.mainloop()
